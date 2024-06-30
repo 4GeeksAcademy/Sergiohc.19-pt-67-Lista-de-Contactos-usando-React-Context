3a73e685285e3634247596a0aec9e7f135640cf6
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-06-30 16:59:14 +0000

    se han cambiado algunos estilos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Home</span>
+				<span className="navbar-brand mx-2 mb-0 h1">Home</span>
 			</Link>
 
 		</nav>
